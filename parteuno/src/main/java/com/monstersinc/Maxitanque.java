9c1db24e49fd33e401de108239577e34f563404f
Adrián Bonilla <53308936+boruroad@users.noreply.github.com>
2023-11-02 23:44:42 -0600

    Maxitanque.java

---
+++ parteuno/src/main/java/com/monstersinc/Maxitanque.java
@@ -0,0 +1,19 @@
+package com.monstersinc;
+
+public class Maxitanque implements Tanque {
+    private final int capacidad;
+
+    public Maxitanque(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    @Override
+    public int obtenerCapacidad() {
+        return capacidad;
+    }
+
+    @Override
+    public String obtenerTipo() {
+        return "Maxitanque";
+    }
+}
